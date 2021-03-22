9481ca26341b4e0965edae253bed058f51d60f8a
resufer <66846932+resufer@users.noreply.github.com>
2021-03-22 22:13:04 +0300

    Update script.js

--- canvas/1/script.js
+++ canvas/1/script.js
@@ -1,28 +1,15 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
-let canWid = document.documentElement.clientWidth;
-let canHei = document.documentElement.clientHeight;
-canvas.width = canWid - 5;
-canvas.height = canHei - 8;
+let width = document.documentElement.clientWidth;
+let height = document.documentElement.clientHeight;
+canvas.width = width;
+canvas.height = height;
+
+let x = 0, y = 0, z = 10;
 ctx.moveTo(0, 0);
 ctx.lineWidth = 3;
-let x = 0, y = 0, z = 10;
 ctx.strokeStyle = 'black';
 
-
-let keyAction = {
-	13: "enter",
-	16: "shift",
-	32: "space",
-	37: "left",
-	38: "up",
-	39: "right",
-	40: "down",
-	69: 'e',
-	83: 's',
-	68: 'd',
-	87: 'w'
-};
 
 let enter = function () {
 	let en1 = prompt('1 - цвет, 2 - длина, 3 - ластик');
@@ -32,7 +19,7 @@ let enter = function () {
 	}
 	else if (en1 == '2') {
 		let strokeLength = prompt('Введите длину');
-		z = Number(strokeLength);
+		z = +(strokeLength);
 	}
 	else if (en1 == '3') {
 		let eraserConfirm = confirm('Стереть всё?')
@@ -42,65 +29,48 @@ let enter = function () {
 
 let eraser = function () {
 	ctx.closePath();
-	ctx.clearRect(0, 0, canWid, canHei);
+	ctx.clearRect(0, 0, width, height);
 }
 
-let keyPress = function () {
-	let key;
-	if (window.event) {
-		key = keyAction[window.event.keyCode];
-	}
-	switch (key) {
-		case 'up':
+let keyPress = function (e) {
+	switch (e.key) {
+		case 'ArrowUp':
 			y -= z;
-			ctx.lineTo(x, y);
-			ctx.stroke();
 			break;
-		case 'down':
+		case 'ArrowDown':
 			y += z;
-			ctx.lineTo(x, y);
-			ctx.stroke();
 			break;
-		case 'left':
+		case 'ArrowLeft':
 			x -= z;
-			ctx.lineTo(x, y);
-			ctx.stroke();
 			break;
-		case 'right':
+		case 'ArrowRight':
 			x += z;
-			ctx.lineTo(x, y);
-			ctx.stroke();
 			break;
 
 		case 'e':
 			y -= z;
 			x += z;
-			ctx.lineTo(x, y);
-			ctx.stroke();
 			break;
 		case 's':
 			y += z;
 			x -= z;
-			ctx.lineTo(x, y);
-			ctx.stroke();
 			break;
 		case 'w':
 			x -= z;
 			y -= z;
-			ctx.lineTo(x, y);
-			ctx.stroke();
 			break;
 		case 'd':
 			x += z;
 			y += z;
-			ctx.lineTo(x, y);
-			ctx.stroke();
 			break;
 
-		case 'enter':
+		case 'Enter':
 			enter();
 			break;
 	}
+
+	ctx.lineTo(x, y);
+	ctx.stroke();
 }
 
 window.onkeydown = keyPress;
