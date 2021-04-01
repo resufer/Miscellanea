9533651c173bda4e3adc1e30431916d4bd0d1ce3
resufer <66846932+resufer@users.noreply.github.com>
2021-04-01 06:48:16 +0300

    Update script.js

--- stopPropagation/script.js
+++ stopPropagation/script.js
@@ -11,16 +11,16 @@ let allColors = ['rgb(255, 255, 255)', 'rgb(255, 0, 0)', 'rgb(252, 193, 2)', 'rg
   'rgb(0, 0, 255)', 'rgb(128, 0, 128)', 'rgb(0, 0, 0)'];
 let colors = allColors;
 
+
 let inputRange = document.getElementById('range');
 let inputValue;
-
 inputRange.onmousedown = (e) => {
   inputValue = inputRange.value;
   inputRange.onmouseup = (e) => {
     inputValue = inputRange.value;
-    colors = allColors.slice(0, allColors.length + 1 - inputValue)
-  }
-}
+    colors = allColors.slice(0, allColors.length + 1 - inputValue);
+  };
+};
 
 
 let checkboxState = false;
@@ -28,90 +28,25 @@ let checkbox = document.getElementById('checkbox');
 
 checkbox.onclick = () => {
   checkboxState = !checkboxState;
-}
+};
 
 
+let addClicksEvent = (arr) => {
+  arr[0].onclick = (e) => {
+    let currentColor = getComputedStyle(arr[0])['background-color'];
+    let index = colors.indexOf(currentColor);
+    if (index === colors.length - 1) {
+      index = -1;
+    }
+    arr[0].style.backgroundColor = colors[index + 1];
 
+    if (checkboxState) e.stopPropagation();
+  };
 
-main.onclick = (e) => {
-  let currentColor = getComputedStyle(main)['background-color'];
-  let index = colors.indexOf(currentColor);
-  if (index === colors.length - 1) {
-    index = -1;
+  if (arr.length === 1) {
+    return;
+  } else {
+    return clickOn(arr.slice(1));
   }
-  main.style.backgroundColor = colors[index + 1];
-
-  if (checkboxState) e.stopPropagation();
-}
-
-
-red.onclick = (e) => {
-  let currentColor = getComputedStyle(red)['background-color'];
-  let index = colors.indexOf(currentColor);
-  if (index === colors.length - 1) {
-    index = -1;
-  }
-  red.style.backgroundColor = colors[index + 1];
-
-  if (checkboxState) e.stopPropagation();
-}
-
-
-yellow.onclick = (e) => {
-  let currentColor = getComputedStyle(yellow)['background-color'];
-  let index = colors.indexOf(currentColor);
-  if (index === colors.length - 1) {
-    index = -1;
-  }
-  yellow.style.backgroundColor = colors[index + 1];
-
-  if (checkboxState) e.stopPropagation();
-}
-
-
-green.onclick = (e) => {
-  let currentColor = getComputedStyle(green)['background-color'];
-  let index = colors.indexOf(currentColor);
-  if (index === colors.length - 1) {
-    index = -1;
-  }
-  green.style.backgroundColor = colors[index + 1];
-
-  if (checkboxState) e.stopPropagation();
-}
-
-
-blue.onclick = (e) => {
-  let currentColor = getComputedStyle(blue)['background-color'];
-  let index = colors.indexOf(currentColor);
-  if (index === colors.length - 1) {
-    index = -1;
-  }
-  blue.style.backgroundColor = colors[index + 1];
-
-  if (checkboxState) e.stopPropagation();
-}
-
-
-purple.onclick = (e) => {
-  let currentColor = getComputedStyle(purple)['background-color'];
-  let index = colors.indexOf(currentColor);
-  if (index === colors.length - 1) {
-    index = -1;
-  }
-  purple.style.backgroundColor = colors[index + 1];
-
-  if (checkboxState) e.stopPropagation();
-}
-
-
-black.onclick = (e) => {
-  let currentColor = getComputedStyle(black)['background-color'];
-  let index = colors.indexOf(currentColor);
-  if (index === colors.length - 1) {
-    index = -1;
-  }
-  black.style.backgroundColor = colors[index + 1];
-
-  if (checkboxState) e.stopPropagation();
-}
+};
+addClicksEvent([main, red, yellow, green, blue, purple, black]);
