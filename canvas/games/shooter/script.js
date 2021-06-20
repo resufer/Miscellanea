f2586d6b6800f0b3bd2c42d2a09a58067bbab565
resufer <66846932+resufer@users.noreply.github.com>
2021-06-20 14:03:27 +0300

    Create script.js

---
+++ canvas/games/shooter/script.js
@@ -0,0 +1,6 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let canWid = document.documentElement.clientWidth;
+let canHei = document.documentElement.clientHeight;
+canvas.width = canWid;
+canvas.height = canHei;
