d6d7d54c9a7c6de5e2855946d8955e2b37645cf1
resufer <66846932+resufer@users.noreply.github.com>
2021-06-20 14:04:45 +0300

    Update script.js

--- canvas/games/shooter/script.js
+++ canvas/games/shooter/script.js
@@ -4,3 +4,12 @@ let canWid = document.documentElement.clientWidth;
 let canHei = document.documentElement.clientHeight;
 canvas.width = canWid;
 canvas.height = canHei;
+
+let x = y = 100, direction;
+
+let coordinates = {
+  'point1': { x: 100, y: 100 },
+  'point2': { x: 120, y: 100 },
+  'point3': { x: 120, y: 120 },
+  'point4': { x: 100, y: 120 }
+};
